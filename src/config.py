40a10b47474902849c0bbf382f63991d0584b514
chiragnm-ai <chiragnm@gmail.com>
2026-03-29 20:09:31 -0700

    Create config.py
    Create the config file

---
+++ src/config.py
@@ -0,0 +1,5 @@
+# Configuration constants
+DATA_PATH = "https://raw.githubusercontent.com/george-mountain/house-price-prediction/master/data/kc_house_data.csv"
+FEATURES = ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 'condition', 'grade']
+TARGET = 'price'
+RANDOM_STATE = 42
